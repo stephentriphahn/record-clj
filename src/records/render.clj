34a1bc290770d93e9fa52b6a8c7e334ec25e8c73
Stephen Triphahn <stephentriphahn@gmail.com>
2019-01-27 15:05:50 -0500

    refactored render namespace, basic implementations of render and parse complete

--- src/records/render.clj
+++ src/records/render.clj
@@ -1,32 +1,55 @@
 (ns records.render
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import (java.text SimpleDateFormat)
+           (java.time.format DateTimeFormatter)))
 
-(defmulti render-by (fn [order-by _]
-                   (str/lower-case order-by)))  ;; lowercase for dispatching
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  Record lens functions
 
-(def sort-by-lastname (partial sort-by #(get % "LastName")))
+(defn last-name
+  [record]
+  (get record "LastName"))
+
+(defn date-of-birth
+  [record]
+  (get record "DateOfBirth"))
+
+(defn gender
+  [record]
+  (get record "Gender"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  Constants
+
+(def formatter (SimpleDateFormat. "mm/dd/yyyy"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  Implementation
+
+(defmulti render-by (fn [order-by _] ;; lowercase for dispatching
+                      (str/lower-case order-by)))
+
+(defn- concat-genders
+  [{:strs [Male Female] :as m}]
+  (concat Female Male))
 
 (defn- sort-genders
   [data]
   (-> data
-      (update "Male" sort-by-lastname)
-      (update "Female" sort-by-lastname)))
+      (update "Male" (partial sort-by last-name))
+      (update "Female" (partial sort-by last-name))
+      concat-genders))
 
 (defmethod render-by "gender"
   [_ data]
   (->> data
-       (group-by #(get % "Gender"))
-       sort-genders
-       vals
-       (apply concat)))
+       (group-by gender)
+       sort-genders))
 
 (defmethod render-by "lastname"
   [_ data]
-  (sort-by #(get % "LastName") #(compare %2 %1) data))
-
-
+  (sort-by last-name #(compare %2 %1) data))
 
 (defmethod render-by "dateofbirth"
   [_ data]
-  ;;todo sort by birthday
-  data)
+  (sort-by (comp #(.parse formatter %) date-of-birth) data))
